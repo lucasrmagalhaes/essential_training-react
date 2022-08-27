df8fa7dbe426225a5ea79f721b4a7986bc85ed4d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-27 15:33:20 -0300

    Incorporating the Link component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 export function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      
       <h1>My Website</h1>
     </div>
   )
@@ -11,6 +17,11 @@ export function Home() {
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      
       <h1>About Us</h1>
     </div>
   )
@@ -19,6 +30,11 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
       <h1>Contact Us</h1>
     </div>
   )
