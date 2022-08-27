6f800a255924f5fca071101da044aa20c520e9c4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-27 13:33:12 -0300

    Understanding the useState Hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
+import { useState } from "react";
 
-/**
-const [firstCity, secondCity, thirdCity] = ["Tokyo", "Tahoe City", "Bend"];
+function App() {
+  const [emotion, setEmotion] = useState("happy");
 
-console.log("firstCity", firstCity);
-console.log("secondCity", secondCity);
-console.log("thirdCity", thirdCity);
-*/
-
-function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current emotion is {emotion}</h1>
+
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App library="React" />
+    <App />
   </React.StrictMode>,
 
   document.getElementById('root')
