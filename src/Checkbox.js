13d3223a0c0266e9a0478733221512fcdbf6d775
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-27 16:20:45 -0300

    Testing events with React Testing Library

---
+++ src/Checkbox.js
@@ -0,0 +1,23 @@
+import { useReducer } from "react";
+
+export function Checkbox() {
+  const [checked, setChecked] = useReducer(
+    (checked) => !checked,
+    false
+  );
+
+  return (
+    <>
+      <label htmlFor="checked">
+        { checked ? "checked" : "not checked" }
+      </label>
+
+      <input
+        id="checked"
+        type="checkbox"
+        value={checked}
+        onChange={setChecked}
+      />
+    </>
+  );
+}
